7f9964bb4fea542e8dc169a37b20a9f47a0539ab
vnoook <noook@yandex.ru>
2024-01-06 12:48:01 +0700

    #50 add pandas experiment

--- panda_experiments.py
+++ panda_experiments.py
@@ -1,16 +1,21 @@
 import pandas as pd
 
-# headers = ['sgtin', 'status', 'withdrawal_type', 'batch', 'expiration_date', 'gtin', 'prod_name', 'last_tracing_op_date']
-headers = ['sgtin', 'status', 'batch']
+# headers = ['sgtin', 'status', 'withdrawal_type', 'batch', 'expiration_date',
+#            'gtin', 'prod_name', 'last_tracing_op_date']
+headers = ['sgtin', 'status']
 
-df = pd.read_csv(r'c:\soft\python3\__programki__\nkvd_medbalance\fd_pd.csv',
+df = pd.read_csv(
+                 # r'c:\soft\python3\__programki__\nkvd_medbalance\fd_pd.csv',
+                 r'c:\soft\python3\__programki__\nkvd_medbalance\fd_pd_full.csv',
                  # names=['sgtin', 'status'],
                  # delimiter=','
                  )
 
-# print(df.to_string(), sep='\n')
+# df.dropna()
+print(df.to_string(), sep='\n')
 
-print(df[headers])
+# print(df['sgtin', 'status'].to_string())
+# print(df['sgtin', 'status'])
 
 # for val in df.iterrows():
 #     print(val)
