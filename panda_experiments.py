ea25ce80022083375f64dc3be77272be4ca74e0a
vnoook <noook@yandex.ru>
2024-01-08 13:16:28 +0700

    #55 add pandas experiment

--- panda_experiments.py
+++ panda_experiments.py
@@ -4,7 +4,7 @@ import pandas as pd
 #            'gtin', 'prod_name', 'last_tracing_op_date']
 headers = ['sgtin', 'status']
 
-df = pd.read_csv(
+df_all = pd.read_csv(
                  r'c:\soft\python3\__programki__\nkvd_medbalance\fd_pd.csv',
                  # r'c:\soft\python3\__programki__\nkvd_medbalance\fd_pd_full.csv',
                  # names=headers,
@@ -16,23 +16,26 @@ df = pd.read_csv(
                  # doublequote=False,
                  )
 
-print(df.to_string())
+print(df_all.to_string())
 # print()
-# print(df)
-print()
-# print(df[headers])
+df1 = df_all[headers]
+print(df1)
 
-df_new1 = df.pivot_table(['sgtin'],['status'], aggfunc='min', fill_value = 0)
-print(df_new1.to_string())
-df_new2 = df.pivot_table(['sgtin'],['status'], aggfunc='max', fill_value = 0)
-print(df_new2.to_string())
-df_new3 = df.pivot_table(['status'],['sgtin'], aggfunc='count', fill_value = 0)
-print(df_new3.to_string())
-df_new4 = df.pivot_table(['sgtin'],['status'], aggfunc='count', fill_value = 0)
-print(df_new4.to_string())
+df1 = df_all[headers]
+print(df1)
+
+# df_new1 = df.pivot_table(['sgtin'],['status'], aggfunc='min', fill_value = 0)
+# print(df_new1.to_string())
+# df_new2 = df.pivot_table(['sgtin'],['status'], aggfunc='max', fill_value = 0)
+# print(df_new2.to_string())
+# df_new3 = df.pivot_table(['status'],['sgtin'], aggfunc='count', fill_value = 0)
+# print(df_new3.to_string())
+# df_new4 = df.pivot_table(['sgtin'],['status'], aggfunc='count', fill_value = 0)
+# print(df_new4.to_string())
 
 # df_new2 = df.pivot_table(['sgtin'],['status'], aggfunc='sum', fill_value = 0)
-# sumtbl = SampleAccounts.pivot_table(['inf_confirm_date'],  ['tcs_customer_id','open_date','final_pmt_date','credit_limit','currency'], aggfunc='max')
+# sumtbl = SampleAccounts.pivot_table(['inf_confirm_date'],  ['tcs_customer_id','open_date','final_pmt_date',
+#                                      'credit_limit','currency'], aggfunc='max')
 # data.pivot_table('PassengerId', 'Pclass', 'Survived', 'count').plot(kind='bar', stacked=True)
 # data.pivot_table('PassengerId', ['SibSp'], 'Survived', 'count').plot(ax=axes[0], title='SibSp')
 # data.pivot_table('PassengerId', ['Parch'], 'Survived', 'count').plot(ax=axes[1], title='Parch')
