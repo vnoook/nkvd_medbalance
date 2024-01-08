f357b6582aa9afb3ab5c3d50b7629f043e05151c
vnoook <noook@yandex.ru>
2024-01-08 19:25:54 +0700

    #57 add pandas experiment

--- panda_experiments.py
+++ panda_experiments.py
@@ -17,24 +17,35 @@ df_all = pd.read_csv(
                  )
 
 print(df_all.to_string())
-# print()
+print()
+
 df1 = df_all[['sgtin', 'status']]
 print(df1)
+print()
 
 df2 = df_all[['sgtin', 'gtin']]
 print(df2)
+print()
 
 df3 = df_all[['sgtin', 'status', 'sell_name', 'prod_name']]
 print(df3)
+print()
 
-# df_new1 = df.pivot_table(['sgtin'],['status'], aggfunc='min', fill_value = 0)
-# print(df_new1.to_string())
-# df_new2 = df.pivot_table(['sgtin'],['status'], aggfunc='max', fill_value = 0)
-# print(df_new2.to_string())
-# df_new3 = df.pivot_table(['status'],['sgtin'], aggfunc='count', fill_value = 0)
-# print(df_new3.to_string())
-# df_new4 = df.pivot_table(['sgtin'],['status'], aggfunc='count', fill_value = 0)
-# print(df_new4.to_string())
+df_new1 = df1.pivot_table(['sgtin'],['status'], aggfunc='min', fill_value = 0)
+print(df_new1.to_string())
+print()
+
+df_new2 = df1.pivot_table(['sgtin'],['status'], aggfunc='max', fill_value = 0)
+print(df_new2.to_string())
+print()
+
+df_new3 = df1.pivot_table(['status'],['sgtin'], aggfunc='count', fill_value = 0)
+print(df_new3.to_string())
+print()
+
+df_new4 = df1.pivot_table(['sgtin'],['status'], aggfunc='count', fill_value = 0)
+print(df_new4.to_string())
+print()
 
 # df_new2 = df.pivot_table(['sgtin'],['status'], aggfunc='sum', fill_value = 0)
 # sumtbl = SampleAccounts.pivot_table(['inf_confirm_date'],  ['tcs_customer_id','open_date','final_pmt_date',
