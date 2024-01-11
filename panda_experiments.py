4c6133db6fbd23bab5d4bd516b865a8548835328
vnoook <noook@yandex.ru>
2024-01-11 21:15:00 +0700

    #62 add pandas experiment

--- panda_experiments.py
+++ panda_experiments.py
@@ -1,48 +1,31 @@
 import pandas as pd
 
+# названия колонок для чтения в csv
 headers = ['prod_name', 'full_prod_name','sgtin']
 
+# прочитать весь файл
 df_all = pd.read_csv(
                  r'fd_pd.csv',
                  # r'fd_pd_full.csv',
-                 # r'fd_pd_new.csv',
-                 # r'8eb6f4c7-5407-4f5f-9ace-7da6cf4a6ea7-0.csv',
                  dtype=object)
 
+# вывести весь файл
 # print(df_all.to_string())
 # print()
 
-df1 = df_all[headers]
-print(df1.to_string())
-print()
-
-df1_new1 = df1.pivot_table(['prod_name'], ['full_prod_name'], aggfunc='count', fill_value = 0)
-print(df1_new1.to_string())
-print()
-
-df1_new2 = df1.pivot_table(['full_prod_name'], ['prod_name'], aggfunc='count', fill_value = 0)
-print(df1_new2.to_string())
-print()
-
-
-# df_new2 = df1.pivot_table(['sgtin'], ['status'], aggfunc='max', fill_value = 0)
-# print(df_new2.to_string())
-# print()
-#
-# df_new3 = df1.pivot_table(['status'], ['sgtin'], aggfunc='count', fill_value = 0)
-# print(df_new3.to_string())
-# print()
-#
-# df_new4 = df1.pivot_table(['sgtin'], ['status'], aggfunc='count', fill_value = 0)
-# print(df_new4.to_string())
+# выбрать нужные колонки
+df = df_all[headers]
+# print(df.to_string())
 # print()
 
+# # посчитать количество prod_name
+# q_prod_name = df.pivot_table('full_prod_name', 'prod_name', aggfunc='count', fill_value = 0)
+# print(q_prod_name.to_string())
+# print()
 
+# подсчёт full_prod_name в колонке относительно prod_name
+df_group1 = df.pivot_table(['prod_name'], ['prod_name', 'full_prod_name'], aggfunc='count', fill_value = 0)
+print(df_group1.to_string())
+print()
 
-
-# df_new2 = df.pivot_table(['sgtin'],['status'], aggfunc='sum', fill_value = 0)
-# sumtbl = SampleAccounts.pivot_table(['inf_confirm_date'],  ['tcs_customer_id','open_date','final_pmt_date',
-#                                      'credit_limit','currency'], aggfunc='max')
-# data.pivot_table('PassengerId', 'Pclass', 'Survived', 'count').plot(kind='bar', stacked=True)
-# data.pivot_table('PassengerId', ['SibSp'], 'Survived', 'count').plot(ax=axes[0], title='SibSp')
-# data.pivot_table('PassengerId', ['Parch'], 'Survived', 'count').plot(ax=axes[1], title='Parch')
+df_group1.to_excel('output.xlsx')
