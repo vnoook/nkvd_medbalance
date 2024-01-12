7e181ee50dc9d9538114b431c5084ffe1c854421
noook <noook@yandex.ru>
2024-01-12 18:08:54 +0700

    #64 add pandas experiment

--- panda_experiments.py
+++ panda_experiments.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 # названия колонок для чтения в csv
-headers = ['prod_name', 'full_prod_name','sgtin']
+headers = ['prod_name', 'full_prod_name']
 
 # прочитать весь файл
 df_all = pd.read_csv(
-                 # r'fd_pd.csv',
-                 r'fd_pd_full.csv',
+                 r'fd_pd.csv',
+                 # r'fd_pd_full.csv',
                  dtype=object)
 
 # вывести весь файл
@@ -20,12 +20,19 @@ df = df_all[headers]
 
 # посчитать количество prod_name
 q_prod_name = df.pivot_table('full_prod_name', 'prod_name', aggfunc='count', fill_value = 0)
-print(q_prod_name.to_string())
-print()
+# print(q_prod_name.to_string())
+# print()
 q_prod_name.to_excel('output1.xlsx')
 
 # подсчёт full_prod_name в колонке относительно prod_name
 df_group1 = df.pivot_table(['prod_name'], ['prod_name', 'full_prod_name'], aggfunc='count', fill_value = 0)
-print(df_group1.to_string())
-print()
+# print(df_group1.to_string())
+# print()
+
+df_group1 = df_group1.reset_index()
+for index, row in df_group1.iterrows():
+    print(index)
+    print(row)
+    print('*'*55)
+
 df_group1.to_excel('output2.xlsx')
