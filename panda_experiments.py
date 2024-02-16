de6962f86afd45102171dd14b8fd884fb0e20779
noook <noook@yandex.ru>
2024-02-16 15:55:45 +0700

    #69 add pandas experiment

--- panda_experiments.py
+++ panda_experiments.py
@@ -25,17 +25,29 @@ q_prod_name = df.pivot_table('full_prod_name', 'prod_name', aggfunc='count', fil
 q_prod_name.to_excel('output1.xlsx')
 
 # подсчёт full_prod_name в колонке относительно prod_name
-df_group1 = df.pivot_table(['prod_name'], ['prod_name', 'full_prod_name', 'status', 'sgtin'], aggfunc='count', fill_value = 0)
-print(df_group1.to_string())
-print()
+df_group1 = df.pivot_table(['prod_name'],
+                           ['prod_name', 'full_prod_name', 'status', 'sgtin'],
+                           aggfunc='count',
+                           fill_value = 0)
+# print(df_group1.to_string())
+# print()
 df_group1.to_excel('output2.xlsx')
-
-exit()
 
 df_group1 = df_group1.reset_index()
 for index, row in df_group1.iterrows():
     print(index)
-    print(row)
-    print('*'*55)
+    # print(row)
+
+    for val in headers:
+        print(f'{row[val] = }')
+    # print(f'{row['prod_name'] = }')
+    # print(f'{row['full_prod_name'] = }')
+    # print(f'{row['status'] = }')
+    # print(f'{row['sgtin'] = }')
+
+    # print()
+    # for i in row:
+    #     print(type(i), i)
+    print('*'*155)
 
 df_group1.to_excel('output2.xlsx')
